b307954868b4c1fd77c03e4f20aa1b9344a3650a
Aizu4 <234679@edu.p.lodz.pl>
2022-12-12 13:17:50 +0100

    Added proper returning of DTO's

--- src/main/java/com/example/pdp/models/FolderDTO.java
+++ src/main/java/com/example/pdp/models/FolderDTO.java
@@ -3,6 +3,12 @@ package com.example.pdp.models;
 public class FolderDTO {
     private String folderName = null;
 
+    public FolderDTO() { }
+
+    public FolderDTO(TreeElement treeElement) {
+        this.folderName = treeElement.getElementName();
+    }
+
     public String getFolderName() {
         return folderName;
     }
